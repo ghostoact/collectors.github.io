6a181ca9e6ea309c241c3d0e2b3e9ed92c9d0001
Jeno <centlan@vip.qq.com>
2021-07-11 19:08:44 +0800

    update:打包路由

---
+++ collectors/src/components/PropItem.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "PropItem",
+  props: {
+  },
+  data() {
+    return {
+     
+    };
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
