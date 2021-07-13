15a64c0628ed5931ee4a57f5c7b602762caad0d0
yaojingzhou <centlan@vip.qq.com>
2021-07-13 14:11:40 +0800

    add:增加CNZZ统计

--- collectors/src/App.vue
+++ collectors/src/App.vue
@@ -19,30 +19,59 @@ import Index from "@/view/Index/Index";
 export default {
   name: "App",
   components: {
-    Index,
+    Index
   },
   data() {
     return {
       menu: [
         {
           name: "奥特曼",
-          url: "/",
+          url: "/"
         },
         {
           name: "特利迦·胜利超越之钥",
-          url: "/HyperKey",
+          url: "/HyperKey"
         },
         {
           name: "泽塔·奥特勋章",
-          url: "/UltraMedal",
-        },
-      ],
+          url: "/UltraMedal"
+        }
+      ]
     };
   },
+  methods: {
+    initCNZZ() {
+      //添加脚本
+      const script = document.createElement("script");
+      script.src =
+        "https://v1.cnzz.com/z_stat.php?id=1280107716&web_id=1280107716";
+      script.language = "JavaScript";
+      script.id = "cnzz";
+      document.body.appendChild(script);
+    },
+    cnzzEvent: function(category, action, label, value, nodeid) {
+      //cnzz事件统计
+      if (window._czc) {
+        czc.push(["_trackEvent", category, action, label, value, nodeid]);
+      }
+    }
+  },
+  mounted() {
+    this.initCNZZ();
+  },
+  watch: {
+    $route() {
+      if (window._czc) {
+        //监听路由变化
+        let location = window.location;
+        let contentUrl = location.pathname + location.hash;
+        let refererUrl = "/";
+        window._czc.push(["_trackPageview", contentUrl, refererUrl]);
+      }
+    }
+  }
 };
 </script>
-
-
 
 <style>
 body {
