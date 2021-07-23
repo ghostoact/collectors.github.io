974f7b168e9aea30de6e9b1f702ec6fb19d7ab8b
yaojingzhou <centlan@vip.qq.com>
2021-07-23 17:20:37 +0800

    add:增加echarts

---
+++ collectors/src/components/EchartsComponents.vue
@@ -0,0 +1,98 @@
+<template>
+  <div style="width:50%;height:200px;"  ref="echarts"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+export default {
+  name: 'EchartsComponents',
+  props: {
+    chartData: {
+      type: Array,
+      default: ()=>[]
+    }
+  },
+  data () {
+    return {
+      count:1
+    }
+  },
+  methods: {
+    drawChart () {
+      const vm = this
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(this.$refs.echarts)
+      // 绘制图表
+      myChart.setOption({
+    title: {
+        text: "用电负荷曲线图",
+        textStyle: {
+            color: "#f2f2f2",
+            fontSize: 12,
+        },
+        top: "5px",
+        right: "20px"
+    },
+    grid: {
+        top: "20px",
+        left: "50px",
+        right: "30px",
+        bottom: "40px"
+    },
+    xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: ["00:00", "03:00", "06:00", "09:00", "12:00", "15:00", "18:00", "21:00"],
+        axisLine: {
+            lineStyle: {
+                color: "#54d9ea",
+            },
+        },
+    },
+    yAxis: {
+        type: "value",
+        axisLine: {
+            lineStyle: {
+                color: "#54d9ea",
+            },
+        },
+        splitLine: {
+            show: false,
+        },
+    },
+    series: [{
+        data: [42, 150, 65, 100, 150, 110, 54, 130],
+        type: "line",
+        lineStyle: {
+            color: "#01fefe",
+        },
+        smooth: true, //true 为平滑曲线，false为直线
+        areaStyle: {
+            //前四个参数代表位置 左下右上，如下表示从上往下渐变色 紫色到暗蓝色，
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: "rgba(8,228,210, 1)",
+                },
+                {
+                    offset: 1,
+                    color: "rgba(8,228,210,  0)",
+                },
+            ]),
+        },
+    }, ],
+})
+    }
+  },
+  computed: {},
+    mounted: function () {
+        const vm = this
+        vm.$nextTick(()=> {
+            vm.drawChart()
+      })
+  },
+  created: () => {}
+}
+</script>
+
+<style scoped>
+</style>
--- collectors/src/view/Trend/Trend.vue
+++ collectors/src/view/Trend/Trend.vue
@@ -1,65 +1,29 @@
 <template>
-  <div>
-    <echarts :options="companyTypeChart" theme="infographic" />
+  <div class="item-list">
+    <EchartsCoponent :chartData="chartData1" />
+    <EchartsCoponent :chartData="chartData2" />
   </div>
 </template>
 
 <script>
-import Echarts from "@/components/Echarts";
+import EchartsCoponent from "@/components/EchartsComponents";
 export default {
   name: "Trend",
   components: {
-    Echarts
+    EchartsCoponent
   },
   data() {
     return {
-      companyTypeChart: {
-        color: ["#61A1A8"],
-        title: {
-          text: "建材单位公司类型",
-          left: "center"
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            // 坐标轴指示器，坐标轴触发有效
-            type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          left: "3%",
-          // right: '4%',
-          bottom: "3%",
-          containLabel: true
-        },
-        label: {
-          show: true,
-          position: "top"
-        },
-        xAxis: {
-          name: "公司类型",
-          type: "category",
-          data: [],
-          axisLabel: {
-            rotate: ["A", "B", "C", "D", "E"]
-          },
-          axisTick: {
-            alignWithLabel: true
-          }
-        },
-        yAxis: {
-          name: "公司数量",
-          type: "value"
-        },
-        series: [
-          {
-            data: [120, 200, 150, 80, 70],
-            barWidth: "25%",
-            type: "bar"
-          }
-        ]
-      }
+      chartData1: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      chartData2: ["苹果", "香蕉", "橘子", "梨子", "樱桃", "哈密瓜"]
     };
+  },
+  components: {
+    EchartsCoponent
+  },
+  mounted: function() {
+    const vm = this;
+    vm.$nextTick(() => {});
   }
 };
 </script>
